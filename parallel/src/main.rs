d835ac19b4dc8e34a3d71a4edce3efff87d7f8cf
Parth Sarkar <ps679@cornell.edu>
2025-04-22 13:23:51 -0400

    need to test with real program

--- parallel/src/main.rs
+++ parallel/src/main.rs
@@ -1,5 +1,10 @@
 mod mutex_worklist;
+use lesson4::framework::{AnalysisFramework, ReachingDefsGeneric};
+use std::collections::{HashMap, HashSet};
 
 fn main() {
+    // let reaching_defs_framework: AnalysisFramework<HashMap<String, HashSet<(usize, usize)>>> =
+    //     AnalysisFramework::new(cfg, init);
+
     println!("Hello, world!");
 }
--- parallel/src/mutex_worklist.rs
+++ parallel/src/mutex_worklist.rs
@@ -1,33 +1,31 @@
 use itertools::Itertools;
+use lesson2::BasicBlock;
 use lesson4::framework::{AnalysisFramework, DataFlowAnalysis};
 use std::{
     collections::{HashMap, VecDeque},
     fmt::Debug,
     ops::{Deref, DerefMut},
-    sync::{Arc, Mutex, RwLock, RwLockReadGuard, RwLockWriteGuard},
-    thread::{self, JoinHandle},
+    sync::{Arc, Mutex, MutexGuard, RwLock, RwLockReadGuard, RwLockWriteGuard},
+    thread::{self},
 };
 
 const NUM_WORKERS: u8 = 5;
 
 /// Trait required to extend functionality of externally-defined AnalysisFramework struct.
-/// Generic type `T` is meant to represent the elements of the lattice.
-pub trait MutexFixpoint<'b, 'c, T>
+/// Generic type `T` is meant to represent the elements of the lattice. In addition
+/// to existing trait impls, T is required to implement Send and Sync.
+pub trait MutexFixpoint<T>
 where
     T: Default + Clone + PartialEq + Debug + Send + Sync,
 {
-    fn worklist(&'b mut self, analysis: impl (DataFlowAnalysis<T>) + Send + Sync + 'c) -> ();
+    fn worklist(&mut self, analysis: impl (DataFlowAnalysis<T>) + Send + Sync) -> ();
 }
 
-impl<'b, 'c, T> MutexFixpoint<'b, 'c, T> for AnalysisFramework<T>
+impl<T> MutexFixpoint<T> for AnalysisFramework<T>
 where
-    T: Default + Clone + PartialEq + Debug + Send + Sync + 'c,
-    'c: 'static,
+    T: Default + Clone + PartialEq + Debug + Send + Sync,
 {
-    fn worklist(&'b mut self, analysis: impl (DataFlowAnalysis<T>) + Send + Sync + 'c) {
-        // define vec to force completion of threads later
-        let mut handles: Vec<JoinHandle<()>> = vec![];
-
+    fn worklist(&mut self, analysis: impl (DataFlowAnalysis<T>) + Send + Sync) {
         // use access to functions in `analysis`
         let shareable_analysis = Arc::new(analysis);
 
@@ -58,74 +56,103 @@ where
             .collect_tuple()
             .unwrap();
 
+        // shared memory of basic blocks array
+        let shareable_blocks: Arc<Vec<BasicBlock>> = Arc::new(self.cfg.blocks.clone());
+
         // init threads
-        for _ in 0..NUM_WORKERS {
-            // pass an atomic reference of each object to each thread
-            let (worklist, succs, preds, ins, outs, analysis) = (
-                Arc::clone(&shareable_worklist),
-                Arc::clone(&shareable_succs),
-                Arc::clone(&shareable_preds),
-                Arc::clone(&shareable_ins),
-                Arc::clone(&shareable_outs),
-                Arc::clone(&shareable_analysis),
-            );
-            let handle = thread::spawn(move || {
-                let worklist_lock = worklist.lock().unwrap();
-                match worklist_lock.front() {
-                    None => drop(worklist_lock),
-                    Some(block_ref) => {
-                        // release lock on worklist queue
-                        let block = *block_ref;
-                        drop(worklist_lock);
+        thread::scope(|spawner| {
+            for _ in 0..NUM_WORKERS {
+                // pass an atomic reference of each object to each thread
+                let (worklist, succs, preds, ins, outs, analysis, blocks) = (
+                    Arc::clone(&shareable_worklist),
+                    Arc::clone(&shareable_succs),
+                    Arc::clone(&shareable_preds),
+                    Arc::clone(&shareable_ins),
+                    Arc::clone(&shareable_outs),
+                    Arc::clone(&shareable_analysis),
+                    Arc::clone(&shareable_blocks),
+                );
+                spawner.spawn(move || {
+                    // acquire lock to pop block off of worklist queue
+                    let worklist_lock: MutexGuard<VecDeque<usize>> = worklist.lock().unwrap();
 
-                        // // acquire read lock on out[p] for all p
-                        let out_read_guards: Vec<RwLockReadGuard<T>> = match preds.get(&block) {
-                            None => vec![],
-                            Some(p) => p.iter().map(|v| *v).collect(),
+                    // now, we have a lock
+                    match worklist_lock.front() {
+                        None => drop(worklist_lock),
+                        Some(block_ref) => {
+                            // release lock on worklist queue
+                            let block = *block_ref;
+                            drop(worklist_lock);
+
+                            // acquire read lock on out[p] for all p
+                            let out_read_guards: Vec<RwLockReadGuard<T>> =
+                                match preds.get(&block) {
+                                    None => vec![],
+                                    Some(p) => p.iter().map(|v| *v).collect(),
+                                }
+                                .into_iter()
+                                .map(|pred: usize| {
+                                    let read_guard = outs[pred].read().unwrap();
+                                    read_guard
+                                })
+                                .collect();
+
+                            // get ownership over these by cloning
+                            let incoming_values = out_read_guards
+                                .iter()
+                                .map(|guard: &RwLockReadGuard<T>| {
+                                    let guard_deref = RwLockReadGuard::deref(guard);
+                                    guard_deref.clone()
+                                })
+                                .collect_vec();
+
+                            // read access to in[b]
+                            let in_b_read_guard: RwLockReadGuard<T> = ins[block].read().unwrap();
+                            let in_b: &T = in_b_read_guard.deref();
+
+                            // merge and drop read access to in[b]
+                            let merge_output: T = analysis.merge(in_b, incoming_values);
+                            drop(in_b_read_guard);
+
+                            // acquire write access to in[b] and write merge output to it
+                            let mut in_b_write_guard = ins[block].write().unwrap();
+                            let in_b: &mut T = in_b_write_guard.deref_mut();
+                            *in_b = merge_output;
+                            drop(in_b_write_guard);
+
+                            // drop read access of each out[p] after finished merging
+                            out_read_guards.into_iter().for_each(drop);
+
+                            // acquire write access to out[b] & read access to in[b]
+                            let mut out_b_write_guard: RwLockWriteGuard<T> =
+                                outs[block].write().unwrap();
+                            let in_b_read_guard: RwLockReadGuard<T> = ins[block].read().unwrap();
+
+                            // do transfer
+                            let in_b: &T = in_b_read_guard.deref();
+                            let new_out =
+                                analysis.transfer(in_b, blocks.get(block).unwrap(), block);
+
+                            // compare results
+                            if !(out_b_write_guard.deref().eq(&new_out)) {
+                                // write to out[b]
+                                let out_b = out_b_write_guard.deref_mut();
+                                *out_b = new_out;
+
+                                let block_succs = match succs.get(&block) {
+                                    None => vec![],
+                                    Some(p) => p.iter().map(|v| *v).collect(),
+                                };
+
+                                // write successors to worklist
+                                let mut worklist_lock: MutexGuard<VecDeque<usize>> =
+                                    worklist.lock().unwrap();
+                                worklist_lock.extend(block_succs);
+                            }
                         }
-                        .into_iter()
-                        .map(|pred: usize| {
-                            let read_guard = outs[pred].read().unwrap();
-                            read_guard
-                        })
-                        .collect();
-
-                        // get ownership over these by cloning
-                        let incoming_values = out_read_guards
-                            .iter()
-                            .map(|guard| {
-                                let guard_deref = RwLockReadGuard::deref(guard);
-                                guard_deref.clone()
-                            })
-                            .collect_vec();
-
-                        // read access to in[b]
-                        let in_b_read_guard = ins[block].read().unwrap();
-                        let in_b = RwLockReadGuard::deref(&in_b_read_guard);
-
-                        // merge
-                        let merge_output = analysis.merge(in_b, incoming_values);
-                        drop(in_b_read_guard);
-
-                        // write access to in[b]
-                        let mut in_b_write_guard = ins[block].write().unwrap();
-                        let in_b = RwLockWriteGuard::deref_mut(&mut in_b_write_guard);
-                        *in_b = merge_output;
-                        drop(in_b_write_guard);
-
-                        // drop read access of each out[p] after finished merging
-                        out_read_guards.into_iter().for_each(drop);
-
-                        ()
                     }
-                }
-            });
-            handles.push(handle);
-        }
-
-        // thunk
-        for handle in handles.into_iter() {
-            handle.join().unwrap();
-        }
+                });
+            }
+        });
     }
 }
