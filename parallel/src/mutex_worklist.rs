1cfc959be3468bc3b62c8c87b85d9ee2efdfc007
Parth Sarkar <ps679@cornell.edu>
2025-04-29 12:12:18 -0400

    added sequential version that will be easier to parallelize

--- parallel/src/mutex_worklist.rs
+++ parallel/src/mutex_worklist.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 use lesson2::BasicBlock;
 use lesson4::framework::{AnalysisFramework, DataFlowAnalysis};
 use std::{
-    collections::{HashMap, VecDeque},
+    collections::{HashMap, HashSet, VecDeque},
     fmt::Debug,
     ops::{Deref, DerefMut},
     sync::{Arc, Mutex, MutexGuard, RwLock, RwLockReadGuard, RwLockWriteGuard},
@@ -19,6 +19,142 @@ where
     T: Default + Clone + PartialEq + Debug + Send + Sync,
 {
     fn worklist(&mut self, analysis: &(impl (DataFlowAnalysis<T>) + Send + Sync)) -> ();
+}
+
+pub trait SequentialFixpoint<T>
+where
+    T: Default + Clone + PartialEq + Debug,
+{
+    fn worklist(&mut self, analysis: &impl (DataFlowAnalysis<T>)) -> ();
+}
+
+pub trait MutexFixpoint2<T>
+where
+    T: Default + Clone + PartialEq + Debug + Send + Sync,
+{
+    fn worklist(&mut self, analysis: &(impl (DataFlowAnalysis<T>) + Send + Sync)) -> ();
+}
+
+impl<T> MutexFixpoint2<T> for AnalysisFramework<T>
+where
+    T: Default + Clone + PartialEq + Debug + Send + Sync,
+{
+    fn worklist(&mut self, analysis: &(impl (DataFlowAnalysis<T>) + Send + Sync)) {}
+}
+
+impl<T> SequentialFixpoint<T> for AnalysisFramework<T>
+where
+    T: Default + Clone + PartialEq + Debug,
+{
+    fn worklist(&mut self, analysis: &impl (DataFlowAnalysis<T>)) -> () {
+        let mut singleton: HashSet<usize> = HashSet::new();
+        singleton.insert(0);
+
+        let (mut worklist, mut visited, mut dependencies): (
+            HashSet<usize>,
+            HashSet<usize>,
+            HashMap<usize, HashSet<usize>>,
+        ) = (singleton.clone(), singleton, HashMap::new());
+
+        loop {
+            match worklist.iter().next() {
+                None => break,
+                Some(block_ref) => {
+                    let (mut c, mut r, mut t): (HashSet<usize>, HashSet<usize>, HashSet<usize>) =
+                        (HashSet::new(), HashSet::new(), HashSet::new());
+
+                    // capture block
+                    let block = *block_ref;
+                    self.ins[block] = if block == 0 {
+                        self.ins[block].clone()
+                    } else {
+                        T::default()
+                    };
+                    worklist.remove(&block);
+
+                    print!("operating on: {block}");
+                    worklist.iter().for_each(|f| print!(", {f}"));
+                    println!();
+
+                    let b_preds = self.cfg.preds(block);
+                    let incoming_values = b_preds
+                        .iter()
+                        .map(|pred_idx: &usize| {
+                            r.insert(*pred_idx);
+                            self.outs[*pred_idx].clone()
+                        })
+                        .collect_vec();
+
+                    r.insert(block);
+
+                    self.ins[block] = analysis.merge(&self.ins[block], incoming_values);
+
+                    let out = analysis.transfer(
+                        &self.ins[block],
+                        self.cfg.blocks.get(block).unwrap(),
+                        block,
+                    );
+
+                    let mut flag = false;
+                    if self.outs[block] != out {
+                        flag = true;
+                    }
+
+                    let b_succs = self.cfg.succs(block);
+
+                    self.outs[block] = out;
+                    if flag {
+                        t.extend(b_succs.clone());
+                    }
+
+                    c.extend(b_succs);
+
+                    let c_mod_v: HashSet<usize> = c.difference(&visited).map(|v| *v).collect();
+                    worklist = worklist.union(&c_mod_v).map(|v| *v).collect();
+                    print!("worklist after uniting with C \\ V: ");
+                    worklist.iter().for_each(|f| print!(", {f}"));
+                    println!();
+                    visited = visited.union(&c).map(|v| *v).collect();
+
+                    r.iter().for_each(|dependency_of_block| {
+                        dependencies
+                            .entry(*dependency_of_block)
+                            .and_modify(|set| {
+                                set.insert(block);
+                            })
+                            .or_insert(HashSet::from_iter(vec![block]));
+                    });
+                    print!("\nt: ");
+
+                    t.iter().for_each(|impacted_by_block| {
+                        print!("{impacted_by_block}, ");
+
+                        match dependencies.get(impacted_by_block) {
+                            None => (),
+                            Some(d_a) => {
+                                worklist.extend(d_a.iter());
+                            }
+                        }
+                    });
+                    println!();
+
+                    print!("\ndependency graph: \n");
+                    dependencies.iter().for_each(|(key, set)| {
+                        print!("  {key}: ");
+                        set.iter().for_each(|s| print!("{s}, "));
+                        println!()
+                    });
+                    println!();
+
+                    print!("worklist after uniting with dependencies: ");
+                    worklist.iter().for_each(|f| print!(", {f}"));
+                    println!();
+                }
+            }
+        }
+
+        ()
+    }
 }
 
 impl<T> MutexFixpoint<T> for AnalysisFramework<T>
@@ -45,6 +181,10 @@ where
         let shareable_succs = Arc::new(succs_map);
         let shareable_preds = Arc::new(preds_map);
 
+        // array of locks to avoid two threads operating on same block b
+        let shareable_collision_list: Arc<Vec<Mutex<()>>> =
+            Arc::new(self.worklist.iter().map(|_| Mutex::new(())).collect());
+
         // shared memory of worklist
         let shareable_worklist: Arc<Mutex<VecDeque<usize>>> =
             Arc::new(Mutex::new(VecDeque::from_iter(self.worklist.drain(..))));
@@ -63,7 +203,7 @@ where
         thread::scope(|spawner| {
             for i in 0..NUM_WORKERS {
                 // pass a reference of each object to each thread
-                let (worklist, succs, preds, ins, outs, analysis, blocks) = (
+                let (worklist, succs, preds, ins, outs, analysis, blocks, collision_list) = (
                     Arc::clone(&shareable_worklist),
                     Arc::clone(&shareable_succs),
                     Arc::clone(&shareable_preds),
@@ -71,6 +211,7 @@ where
                     Arc::clone(&shareable_outs),
                     Arc::clone(&shareable_analysis),
                     Arc::clone(&shareable_blocks),
+                    Arc::clone(&shareable_collision_list),
                 );
                 spawner.spawn(move || {
                     loop {
@@ -86,7 +227,17 @@ where
                             }
                             Some(block) => {
                                 // release lock on worklist queue
-                                // drop(worklist_lock);
+                                print!("Worklist before operation of thread {i}: ");
+                                print!("{block}");
+                                worklist_lock.iter().for_each(|v| print!(", {v}"));
+                                println!();
+                                println!("Thread {i} got block {block}");
+
+                                drop(worklist_lock);
+
+                                // acquire sole right to this block
+                                let collision_guard = collision_list.get(block).unwrap().lock();
+                                println!("Thread {i} got through!");
 
                                 // acquire read lock on out[p] for all p
                                 let out_read_guards: Vec<RwLockReadGuard<T>> =
@@ -122,7 +273,9 @@ where
                                 // acquire write access to in[b] and write merge output to it
                                 let mut in_b_write_guard = ins[block].write().unwrap();
                                 let in_b: &mut T = in_b_write_guard.deref_mut();
+
                                 *in_b = merge_output;
+                                println!("Thread {i} holding block {block} has the following for in[b]: {:#?}", in_b);
                                 drop(in_b_write_guard);
 
                                 // drop read access of each out[p] after finished merging
@@ -131,6 +284,7 @@ where
                                 // acquire write access to out[b] & read access to in[b]
                                 let mut out_b_write_guard: RwLockWriteGuard<T> =
                                     outs[block].write().unwrap();
+
                                 let in_b_read_guard: RwLockReadGuard<T> =
                                     ins[block].read().unwrap();
 
@@ -144,11 +298,12 @@ where
                                 // compare results
                                 if !(out_b_write_guard.deref().eq(&new_out)) {
                                     // write successors to worklist
-                                    // let mut worklist_lock: MutexGuard<VecDeque<usize>> =
-                                    //     worklist.lock().unwrap();
+                                    let mut worklist_lock: MutexGuard<VecDeque<usize>> =
+                                        worklist.lock().unwrap();
 
                                     // write to out[b]
                                     let out_b = out_b_write_guard.deref_mut();
+
                                     *out_b = new_out;
 
                                     let block_succs = match succs.get(&block) {
@@ -157,9 +312,15 @@ where
                                     };
 
                                     worklist_lock.extend(block_succs);
+                                    worklist_lock.push_back(block);
+                                    print!("Worklist after {i} added successors: ");
+                                    worklist_lock.iter().for_each(|v| print!(", {v}"));
+                                    println!();
+                                    drop(worklist_lock);
+                                    drop(out_b_write_guard);
                                 }
-                                drop(worklist_lock);
-                                drop(out_b_write_guard);
+                                drop(collision_guard);
+                                println!("Thread {i} finished!");
                             }
                         }
                     }
